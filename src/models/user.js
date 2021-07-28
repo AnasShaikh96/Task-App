b844519b0c7b03ea427357850be20d89b0ec4767
Anas Shaikh <shaikhanas246@gmail.com>
2021-07-28 11:27:17 +0530

    added dev-tools and changes

---
+++ src/models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    trim: true,
+
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("email is invalid");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("age must be positive number");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error("password cannot contain password");
+      }
+    },
+  },
+});
+
+const work = new User({
+  name: "beluga",
+  email: "beluwuga@bonk.com",
+  age: 14,
+  password: "12233444455555",
+});
+
+work
+  .save()
+  .then((result) => {
+    // console.log(result);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+module.exports = User;
